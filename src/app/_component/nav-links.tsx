2c6a41ad45c05b198e46a14c39c3091433b379a4
yoshi <yoshitakaOkada0214@gmail.com>
2023-12-03 19:09:38 +0900

    Chapter 5 Navigating Between Pages

--- src/app/_component/nav-links.tsx
+++ src/app/_component/nav-links.tsx
@@ -1,5 +1,7 @@
+'use client'
 import { DocumentDuplicateIcon, HomeIcon, UserGroupIcon } from '@heroicons/react/24/outline'
 import { Box, Link, Typography } from '@mui/material'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
 // いったん固定値で書くけど本来はdbから引っ張ってきてね
@@ -10,6 +12,7 @@ const links = [
 ]
 
 export default function NavLinks() {
+  const pathname = usePathname()
   return (
     <>
       {links.map((link) => {
@@ -18,7 +21,12 @@ export default function NavLinks() {
           <Link key={link.name} href={link.href}>
             <Box display='flex' flexDirection='row' marginTop='20px'>
               <LinkIcon style={{ width: '2.5rem', height: '2.5rem', marginRight: '10px' }} />
-              <Typography variant='h6'>{link.name}</Typography>
+              <Typography
+                variant='h6'
+                color={pathname === link.href ? 'primary.main' : 'text.primary'}
+              >
+                {link.name}
+              </Typography>
             </Box>
           </Link>
         )
